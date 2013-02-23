64e56fae38dc4027af73df4d1079b5ecf3a83b4c
Aziz Khoury <bentael@gmail.com>
2013-02-22 20:53:25 -0500

    syntax fix

--- refreshHost.js
+++ refreshHost.js
@@ -9,7 +9,7 @@
 
 var casper = require("casper").create(), email, password;
 
-console.log('email: ' + casper.cli.get(0)) + '\n\n');
+console.log('email: ' + casper.cli.get(0) + '\n\n');
 
 email = casper.cli.get(0);
 password = casper.cli.get(1);

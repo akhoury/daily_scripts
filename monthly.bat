a4a9f4752e3a0dc1d70b747cafe783a27d9064cc
Aziz Khoury <akhoury@MacBook-Pro.home>
2016-07-06 21:28:43 -0400

    update

--- monthly.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d %~dp0
-set EMAIL=your_no-ip_email@something.com
-set PASS=yourpassword
-
-del refreshHost.js
-wget --no-check-certificate https://raw.github.com/akhoury/daily_scripts/master/refreshHost.js
-casperjs refreshHost.js %EMAIL% %PASS% > casper-logs.txt
